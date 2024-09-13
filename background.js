e4f3de18f5373f15fb91e514c4fc609ff4456182
rahmanabdur1 <rahmanabdurr65@gmail.com>
2024-09-13 07:48:57 +0300

    code added

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === "download_image") {
     chrome.downloads.download({
       url: request.url,
+      filename: `facebook_photo_${Date.now()}.jpg`, // Optional: custom filenames
       conflictAction: 'uniquify'
     });
   }
--- content.js
+++ content.js
@@ -1,11 +1,18 @@
 function downloadImages() {
   const images = document.querySelectorAll('img');
+  let foundImages = [];
+
   images.forEach((img) => {
     const url = img.src;
-    if (url) {
+    if (url && !foundImages.includes(url)) {
+      foundImages.push(url);
       chrome.runtime.sendMessage({ action: "download_image", url: url });
     }
   });
+
+  if (foundImages.length === 0) {
+    alert('No images found on this page!');
+  }
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
@@ -13,3 +20,4 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     downloadImages();
   }
 });
+
